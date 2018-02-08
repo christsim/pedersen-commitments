48e13e80a26eaa4b85622c17b1175409119466cb
Chris <chris.tsimogiannis@rmb.co.za>
2018-02-08 14:40:15 +0200

    WIP

--- test/pedersenHD-test.js
+++ test/pedersenHD-test.js
@@ -8,16 +8,19 @@ describe('pedersenHD', () => {
 
     it('should commit to a number and verify it', () => {
         var params = PedersenHD.setup();
+        console.log(params);
         var c1 = PedersenHD.commitTo(params, 10);
         assert(PedersenHD.verify(params, c1.C, c1.r, 10));
     });
 
     it('should be able to add two commitments', () => {
-        var params = PedersenHD.setup();
-        console.log(params);
+        var H = '18d364bf6dbb72333da1d87361e7e02ace8639bc29b52c93c39cc0366f13511d';
+        var params = PedersenHD.setup(H);
+        console.log(H);
         var c1 = PedersenHD.commitTo(params, 10);
-        assert(PedersenHD.verify(params, c1.C, c1.r, 10));
+        var c2 = PedersenHD.commitTo(params, 11);
 
+        var C2 = PedersenHD.add(c1.C, c2.C);
         
 
     })
