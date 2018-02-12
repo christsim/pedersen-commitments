b497d5709318ca623a0e1ef79ec09bbcd35ebdc8
Chris <28301107+christsim@users.noreply.github.com>
2018-02-12 08:11:11 +0200

    wip - adding solidity tests and contract

---
+++ test/pedersen-solidity-test.js
@@ -0,0 +1,17 @@
+const PedersenAbtraction = artifacts.require('Pederson');
+
+let instance;
+
+contract('Pedersen', (accounts) => {
+  beforeEach(async() => {
+    
+  });
+
+  it('can add commitments', async() => {
+    let instance = await PedersenAbtraction.new();
+
+
+  });
+
+  
+});
