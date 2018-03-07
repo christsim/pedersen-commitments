c826e4d133db786bd9a5bf30e00bb0420643239e
Chris <christsim@gmail.com>
2018-03-07 08:12:42 +0200

    moved solidity to another repo

--- migrations/2_pedersen_migration.js
+++
@@ -1,7 +0,0 @@
-var PedersenAbstractions = artifacts.require("./Pedersen.sol");
-var BigNumber = require('bignumber.js');
-
-module.exports = function(deployer) {
-  var zero = new BigNumber(1);
-  deployer.deploy(PedersenAbstractions, [zero, zero]);
-};
